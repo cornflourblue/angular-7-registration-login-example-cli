a29d8a3c3ea17d7a7bc796d5e645f75395d837e2
Jason Watmore <jason@pointblankdevelopment.com.au>
2018-11-04 12:05:46 +1100

    Initial commit

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get(`${environment.apiUrl}/users/${id}`);
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.apiUrl}/users/register`, user);
+    }
+
+    update(user: User) {
+        return this.http.put(`${environment.apiUrl}/users/${user.id}`, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${environment.apiUrl}/users/${id}`);
+    }
+}
